09369721eafce8ddb936613a7389b9e54c9d3404
huanglijun <437028394@qq.com>
2020-04-27 16:02:24 +0800

    update return value

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,19 @@ router.post('/upload',async(ctx)=>{
 	const file = ctx.request.files.file;	// 获取上传文件
 	const reader = fs.createReadStream(file.path);	// 创建可读流
 	const ext = file.name.split('.').pop();		// 获取上传文件扩展名
-	const upStream = fs.createWriteStream(`public/${Math.random().toString()}.${ext}`);		// 创建可写流
+	const crypto = require('crypto');
+	const md5 = crypto.createHash('md5')
+	let file_name = `${Date.now().toString()}` + Math.random().toString();
+	file_name = md5.update(file_name).digest('hex') + `.${ext}`;
+	const file_path = `public/${file_name}`;
+	const upStream = fs.createWriteStream(file_path);		// 创建可写流
 	reader.pipe(upStream);	// 可读流通过管道写入可写流
-	return ctx.body = '上传成功';
+	return ctx.body = {"result":0, "url":"http://localhost:3000/" + file_name};
 })
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: '上传图片'
   })
 })
 
